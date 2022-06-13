848efd48ac7512fd2ab5e3cd1881b56faf02b02b
LNDavid <ldavid@rmacan.com>
2022-06-12 20:49:26 -0400

    page created even if lots of work to go

--- src/team-template.js
+++ src/team-template.js
@@ -17,7 +17,7 @@ function generateTeamTemplate(team)   {
                 officeNumber: team[i].officeNumber
             }    
                 console.log(teamMember);
-                // buildManagerCard();
+                buildManagerCard();
         } else if (team[i[j]].role === "engineer") {
                 teamMember = {
                 role: team[i].role,
@@ -26,7 +26,8 @@ function generateTeamTemplate(team)   {
                 email: team[i].email,
                 github: team[i].github
             }   
-                console.log(teamMember);// buildEngineerCard();
+                console.log(teamMember);
+                buildEngineerCard();
         } else if (team[i].role === "intern") {
                 teamMember = {
                  role: team[i].role,
@@ -36,75 +37,119 @@ function generateTeamTemplate(team)   {
                  school: team[i].officeNumber
                 }     
                 console.log(teamMember);
-               // buildInternCard()
+               buildInternCard();
         }
         }
     
 };
 
-
-// function generateCards(teamMember) {
-
-//  // create employee id cards
-// const buildManagerCard = teamMember => {
-//     role = teamSort(teamMember.role);
-//     name = teamSort(teamMember.name);
-//     id = teamSort(teamMember.id);
-//     email = teamSort(teamMember.email);
-//     officeNumber = teamSort(teamMember.officeNumber);      
-
-// return `    
-// <div class="card manager">                        
-//     <div class='card-header manager'> 
-//     ${teamMember
-//         .filter(({role}) => role)
-//         .map(({ role, name, id, email, officeNumber}) => {
-//         return`                              
-//         <h2 class="ee-name">${name}</h2><br>                            
-//         <h4 class="ee-role">${role}</h4>                        
-//     </div>
-//     <div class="card-body manager">                           
-//         <div class='id'>${id}</div>                            
-//         <div class='email'>${email}</div>                            
-//         <div class='office-number'>${officeNumber}</div>                        
-//     </div> 
-//         `;
-//     })
-// }    
-// </div>
-// })                      
-// `;
-
-// };
-//      return`    
+    // create employee id cards
+    const buildManagerCard = teamMember => {
         
-//     <!DOCTYPE html
-//     <html lang="en">
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta name = "viewport" content="width=device-width, initial-scale-1.0">        
-//         <meta http-equiv="X-UA Compatible" content="ie=edge">        
-//         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">        
-//         <link rel="stylesheet" href="../src/style.css">       
-//         <title>Team-Profile-Generator</title>  
-//     </head>    
-//     <body>       
-//         <header>          
-//             <h1 class="title">My Team</h1>       
-//         </header>       
+    return `    
+        <div class="card manager">                        
+            <div class='card-header manager'> 
+            ${teamMember
+                .filter(({role}) => role)
+                .map(({ role, name, id, email, officeNumber}) => {
+                return`                              
+                <h2 class="ee-name">${name}</h2><br>                            
+                <h4 class="ee-role">${role}</h4>                        
+            </div>
+            <div class="card-body manager">                           
+                <div class='id'>${id}</div>                            
+                <div class='email'>${email}</div>                            
+                <div class='office-number'>${officeNumber}</div>                        
+            </div> 
+                `;
+            })
+        }    
+        </div>
+        })                      
+        `;
+    };
 
-//         <main class="team-board">        
-//             ${buildManagerCard(teamMember)}
+    const buildEngineerCard = teamMember => {
         
-//         </main>
-//     </body>
-//     <script src="../src/script.js"></script>
-//     </html>
-//     `;
+        return `    
+            <div class="card engineer">                        
+                <div class='card-header engineer'> 
+                ${teamMember
+                    .filter(({role}) => role)
+                    .map(({ role, name, id, email, github}) => {
+                    return`                              
+                    <h2 class="ee-name">${name}</h2><br>                            
+                    <h4 class="ee-role">${role}</h4>                        
+                </div>
+                <div class="card-body engineer">                           
+                    <div class='id'>${id}</div>                            
+                    <div class='email'>${email}</div>                            
+                    <div class='github'>${github}</div>                        
+                </div> 
+                    `;
+                })
+            }    
+            </div>
+            })                      
+            `;
+        };
 
-//  }
-// //  generateTeamCards();
-};
+        const buildInternCard = teamMember => {
+        
+            return `    
+                <div class="card intern">                        
+                    <div class='card-header intern'> 
+                    ${teamMember
+                        .filter(({role}) => role)
+                        .map(({ role, name, id, email, school}) => {
+                        return`                              
+                        <h2 class="ee-name">${name}</h2><br>                            
+                        <h4 class="ee-role">${role}</h4>                        
+                    </div>
+                    <div class="card-body intern">                           
+                        <div class='id'>${id}</div>                            
+                        <div class='email'>${email}</div>                            
+                        <div class='school'>${school}</div>                        
+                    </div> 
+                        `;
+                    })
+                }    
+                </div>
+                })                      
+                `;
+            };
+return `
+    
+    <!DOCTYPE html
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name = "viewport" content="width=device-width, initial-scale-1.0">        
+            <meta http-equiv="X-UA Compatible" content="ie=edge">        
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">        
+            <link rel="stylesheet" href="../src/style.css">       
+            <title>Team-Profile-Generator</title>  
+        </head>    
+        <body>       
+            <header>          
+                <h1 class="title">My Team</h1>       
+            </header>       
+    
+            <main class="team-board">        
+                ${buildManagerCard(teamMember)}
+                ${buildEngineerCard(teamMember)}
+                ${buildInternCard(teamMember)}
+            
+            </main>
+        </body>
+        <script src="../src/script.js"></script>
+    </html>
+   `;
+ 
+   
+ };
+
+
 module.exports = generateTeamTemplate;
 
     
